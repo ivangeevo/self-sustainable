046858ed4a9ae2aef22f008137d3024883d08bd4
Ivan Geevo <dbowgaming@gmail.com>
2025-02-24 17:47:26 +0200

    prepare to merge torches branch

--- src/main/java/net/ivangeevo/self_sustainable/item/ModItems.java
+++ src/main/java/net/ivangeevo/self_sustainable/item/ModItems.java
@@ -26,22 +26,6 @@ public class ModItems
     public static final Item BRICK_UNFIRED = registerItem( "brick_unfired",
             new AliasedBlockItem(ModBlocks.BRICK_UNFIRED, new Item.Settings()));
 
-
-    // All items below are unused for now
-    public static final Item WOOL = registerItem("wool", new WoolItem( new Item.Settings().maxCount(64)));
-    public static final Item WOOL_KNIT = registerItem( "wool_knit", new WoolKnitItem(new Item.Settings()));
-
-    public static final Item KNITTING_NEEDLES = registerItem("knitting_needles",
-            new KnittingNeedlesItem(ToolMaterials.WOOD,
-                    new Item.Settings().recipeRemainder(ModItems.KNITTING_NEEDLES)));
-
-    public static final Item KNITTING = registerItem("knitting",
-            new KnittingItem(new Item.Settings()));
-
-    public static final Item WICKER = registerItem("wicker", new Item(new Item.Settings()));
-    public static final Item WICKER_WEAVING = registerItem("wicker_weaving",
-            new WickerWeavingItem(new Item.Settings().maxCount(1)));
-
     public static final Item CRUDE_TORCH_UNLIT = registerItem("crude_torch_unlit", new CrudeTorchItem(ModBlocks.CRUDE_TORCH_UNLIT, ModBlocks.CRUDE_WALL_TORCH_UNLIT, new Item.Settings(), TorchFireState.UNLIT, ModBlocks.crudeTorches));
     public static final Item CRUDE_TORCH_LIT = registerItem("crude_torch_lit", new CrudeTorchItem(ModBlocks.CRUDE_TORCH_LIT, ModBlocks.CRUDE_WALL_TORCH_LIT, new Item.Settings().maxDamage(24000), TorchFireState.LIT, ModBlocks.crudeTorches));
     public static final Item CRUDE_TORCH_SMOULDER = registerItem("crude_torch_smoulder", new CrudeTorchItem(ModBlocks.CRUDE_TORCH_SMOULDER, ModBlocks.CRUDE_WALL_TORCH_SMOULDER, new Item.Settings().maxDamage(24000), TorchFireState.SMOULDER, ModBlocks.crudeTorches));
@@ -50,6 +34,13 @@ public class ModItems
     public static final Item TORCH_UNLIT = registerItem("torch_unlit", new VerticallyAttachableBlockItem(ModBlocks.TORCH_UNLIT, ModBlocks.WALL_TORCH_UNLIT, new Item.Settings(), Direction.DOWN));
 
 
+    // All items below are unused for now
+    public static final Item WOOL = registerItem("wool", new WoolItem( new Item.Settings().maxCount(64)));
+    public static final Item WOOL_KNIT = registerItem( "wool_knit", new WoolKnitItem(new Item.Settings()));
+    public static final Item KNITTING_NEEDLES = registerItem("knitting_needles", new KnittingNeedlesItem(ToolMaterials.WOOD, new Item.Settings().recipeRemainder(ModItems.KNITTING_NEEDLES)));
+    public static final Item KNITTING = registerItem("knitting", new KnittingItem(new Item.Settings()));
+    public static final Item WICKER = registerItem("wicker", new Item(new Item.Settings()));
+    public static final Item WICKER_WEAVING = registerItem("wicker_weaving", new WickerWeavingItem(new Item.Settings().maxCount(1)));
 
     // ****************** //
 

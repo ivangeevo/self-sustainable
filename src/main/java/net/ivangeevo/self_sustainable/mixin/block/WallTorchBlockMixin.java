45f8780ec96a421ae83149b71aff6381aec1d5e3
Ivan Geevo <dbowgaming@gmail.com>
2025-05-20 02:41:48 +0300

    bump changes

--- src/main/java/net/ivangeevo/self_sustainable/mixin/block/WallTorchBlockMixin.java
+++ src/main/java/net/ivangeevo/self_sustainable/mixin/block/WallTorchBlockMixin.java
@@ -1,16 +1,11 @@
 package net.ivangeevo.self_sustainable.mixin.block;
 
-import com.terraformersmc.modmenu.util.mod.Mod;
 import net.ivangeevo.self_sustainable.block.ModBlocks;
-import net.ivangeevo.self_sustainable.block.blocks.CrudeWallTorchBlock;
-import net.ivangeevo.self_sustainable.block.interfaces.Ignitable;
 import net.minecraft.block.*;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.world.World;
-import net.minecraft.world.WorldAccess;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -22,6 +17,7 @@ public abstract class WallTorchBlockMixin extends AbstractTorchBlock {
         super(settings);
     }
 
+    // No particle should display for unlit torches
     @Inject(method = "randomDisplayTick", at = @At("HEAD"), cancellable = true)
     private void stopParticles(BlockState state, World world, BlockPos pos, Random random, CallbackInfo ci) {
         if (state.isOf(ModBlocks.WALL_TORCH_UNLIT)) {

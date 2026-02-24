6dbeba4336244a79cdf9ea1a128aab22a70cb919
ivan <dbowgaming@gmail.com>
2026-02-24 04:19:17 +0200

    Code cleanup & fixes (moved stuff to btwr:sl)

--- src/main/java/org/btwr/self_sustainable/entity/interfaces/added/LivingEntityAdded.java
+++
@@ -1,9 +0,0 @@
-package org.btwr.self_sustainable.entity.interfaces.added;
-
-public interface LivingEntityAdded {
-
-    default void btwr$setItemUseTime(int iCount) {
-        throw new UnsupportedOperationException();
-    }
-
-}

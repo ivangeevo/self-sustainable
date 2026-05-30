8ca592541612c4c71fa988bb684622a54fdcce34
ivan <dbowgaming@gmail.com>
2026-05-30 04:51:35 +0300

    code cleanup

--- src/client/java/org/btwr/self_sustainable/block/entity/render/SmokeOvenBERenderer.java
+++ src/client/java/org/btwr/self_sustainable/block/entity/render/SmokeOvenBERenderer.java
@@ -32,66 +32,9 @@ public class SmokeOvenBERenderer implements BlockEntityRenderer<SmokerOvenBE> {
        // this.renderFuelLevelOverlay(entity, matrices, vertexConsumers, light, overlay);
 
         // Render the item being in the oven
-        this.renderCookItem(entity, matrices, vertexConsumers);
+        //this.renderCookItem(entity, matrices, vertexConsumers);
     }
 
 
-    private void renderCookItem(SmokerOvenBE ovenBE, MatrixStack matrices, VertexConsumerProvider vertexConsumers) {
-        // Get the itemsBeingCooked from the entity
-        ItemStack cookStack = ovenBE.getCookStack();
-        Direction facing = ovenBE.getCachedState().get(SmokerOvenBlock.FACING);
-
-        matrices.push();
-
-        // Move to the center of the block
-        matrices.translate(0.5f, 0.58f, 0.5f);
-
-        // Integrate the visual offset logic
-        applyVisualOffset(matrices, facing);
-
-        // Rotate based on the facing direction
-        RotationAxis rotationAxis = this.getRotationAxis(facing);
-        matrices.multiply(rotationAxis.rotationDegrees(facing.asRotation()));
-
-        // Scale the item to an appropriate size
-        matrices.scale(0.35f, 0.35f, 0.35f);
-
-        // Use the itemRenderer to render the item
-        this.itemRenderer.renderItem(cookStack, ModelTransformationMode.GUI,
-                LightmapTextureManager.pack(8, 15), OverlayTexture.DEFAULT_UV,
-                matrices, vertexConsumers, ovenBE.getWorld(), 1);
-
-        matrices.pop();
-    }
-
-    private void applyVisualOffset(MatrixStack matrices, Direction facing) {
-        float visualOffset = 0.25f;
-
-        switch (facing) {
-            case NORTH:
-                matrices.translate(0.0f, 0.0f, -0.5f + visualOffset);
-                break;
-            case SOUTH:
-                matrices.translate(0.0f, 0.0f, 0.5f - visualOffset);
-                break;
-            case WEST:
-                matrices.translate(-0.5f + visualOffset, 0.0f, 0.0f);
-                break;
-            case EAST:
-                matrices.translate(0.5f - visualOffset, 0.0f, 0.0f);
-                break;
-        }
-    }
-
-    // Get RotationAxis based on facing direction
-    private RotationAxis getRotationAxis(Direction facing) {
-        return switch (facing) {
-            case NORTH -> RotationAxis.POSITIVE_Z;
-            case SOUTH -> RotationAxis.POSITIVE_Z;
-            case WEST -> RotationAxis.POSITIVE_Y;
-            case EAST -> RotationAxis.POSITIVE_Y;
-            default -> RotationAxis.POSITIVE_Y; // Default to Y-axis if facing direction is not recognized
-        };
-    }
 
 }
--- src/main/java/org/btwr/self_sustainable/block/entity/SmokerOvenBE.java
+++ src/main/java/org/btwr/self_sustainable/block/entity/SmokerOvenBE.java
@@ -17,57 +17,8 @@ public class SmokerOvenBE extends AbstractOvenBE {
         super(ModBlockEntities.SMOKER_BRICK, pos, state);
     }
 
-    public static void serverTick(World world, BlockPos pos, BlockState state, @NotNull SmokerOvenBE ovenBE) {
-        ItemStack cookStack = ovenBE.getCookStack();
+    @Override
+    public void tick() {
 
-        boolean bWasBurning = ovenBE.fuelBurnTime > 0;
-        boolean bInvChanged = false;
-
-        SingleStackRecipeInput singleStackRecipeInput = new SingleStackRecipeInput(cookStack);
-        ItemStack cookedStack = ovenBE.matchGetter
-                .getFirstMatch(singleStackRecipeInput, world)
-                .map(recipe -> (recipe.value()).craft(singleStackRecipeInput, world.getRegistryManager()))
-                .orElse(cookStack);
-
-        // Decrease furnace burn time if it's still burning
-        if (ovenBE.fuelBurnTime > 0) {
-            --ovenBE.fuelBurnTime;
-        }
-
-        if ((state.get(LIT) && ovenBE.unlitFuelBurnTime > 0) || ovenBE.lightOnNextUpdate) {
-            ovenBE.fuelBurnTime += ovenBE.unlitFuelBurnTime;
-            ovenBE.unlitFuelBurnTime = 0;
-            ovenBE.lightOnNextUpdate = false;
-        }
-
-        if (ovenBE.isBurning()) {
-            // Increment the cooking time
-            ++ovenBE.cookTime;
-
-            if (ovenBE.cookTime >= ovenBE.cookTimeTotal && cookedStack.isItemEnabled(world.getEnabledFeatures())) {
-                ovenBE.setStack(cookedStack);
-                bInvChanged = true;
-                ovenBE.cookTime = 0;
-                world.updateListeners(pos, state, state, Block.NOTIFY_ALL);
-                world.emitGameEvent(GameEvent.BLOCK_CHANGE, pos, GameEvent.Emitter.of(state));
-            }
-        }
-        else {
-            world.setBlockState(pos, state.with(LIT, false));
-            ovenBE.cookTime = 0;
-        }
-
-        ovenBE.updateVisualFuelLevel();
-
-
-        if (bInvChanged) {
-            markDirty(world, pos, state);
-        }
     }
-
-
-    public static void clientTick(World world, BlockPos pos, BlockState state, SmokerOvenBE ovenBE) {
-        setParticles(world, pos, state);
-    }
-
 }
